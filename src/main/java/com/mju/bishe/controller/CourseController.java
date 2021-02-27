dfd612334dc00ba2ea0d3f6c87afcc51c281fe74
Zhuangjunhui <zhuangjunhui_yg@outlook.com>
2021-02-27 15:50:16 +0800

    1

--- src/main/java/com/mju/bishe/controller/CourseController.java
+++ src/main/java/com/mju/bishe/controller/CourseController.java
@@ -1,9 +1,18 @@
 package com.mju.bishe.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.mju.bishe.common.PageParams;
+import com.mju.bishe.common.Result;
+import com.mju.bishe.common.ResultFactory;
+import com.mju.bishe.entity.Course;
 import com.mju.bishe.service.CourseService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @Api(value = "服务接口", tags = "服务接口")
@@ -12,5 +21,15 @@ import org.springframework.web.bind.annotation.*;
 public class CourseController {
     @Autowired
     private CourseService targetService;
-    
+    /**
+     * 获取分页数据
+     */
+    @ApiOperation(value = "获取分页数据", notes = "获取分页数据")
+    @PostMapping(value = "/list")
+    @ResponseBody
+    public Result<IPage<Course>> list(@RequestBody Map map){
+        QueryWrapper<Course> queryWrapper=new QueryWrapper();
+        PageParams pageParams=new PageParams(map);
+        return ResultFactory.success(targetService.page(pageParams,queryWrapper));
+    }
 }
--- src/main/java/com/mju/bishe/service/CourseService.java
+++ src/main/java/com/mju/bishe/service/CourseService.java
@@ -1,7 +1,9 @@
 package com.mju.bishe.service;
 
 
+
 import com.baomidou.mybatisplus.extension.service.IService;
+
 import com.mju.bishe.entity.Course;
 
 public interface CourseService extends IService<Course> {
